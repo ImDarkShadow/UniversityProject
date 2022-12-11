d6f91b0ba005e15c1b817a0aa5c2e64f65a10b09
Pritam Das <40018547+ImDarkShadow@users.noreply.github.com>
2022-12-11 22:55:13 +0530

    added requirements.txt

--- print.py
+++ print.py
@@ -1,3 +1,10 @@
 import cv2 as cv
 import textwrap
+
+
 def putText(image, text, cords, font, size, color, thickness):
+    lines = textwrap.wrap(text, width=20)
+    for i, line in enumerate(lines):
+        cv.putText(
+            image, line, (cords[0], cords[1] + (i * 25)), font, size, color, thickness)
+    return image
