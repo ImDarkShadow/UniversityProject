04dfd9469a90025be93b2dbac66ce936b6ec26f6
Pritam Das <40018547+ImDarkShadow@users.noreply.github.com>
2023-01-25 15:58:30 +0530

    fix : cleanRawText.py according to complexBG

--- cleanRawText.py
+++ cleanRawText.py
@@ -1,10 +1,17 @@
 import cv2 as cv
+import numpy as np
+import dominantColor
 
 
-def cleanRaw(image, cords, color):
+def cleanRaw(image, cords, compexbg=False):
+    color = (255, 255, 255)
     for i in range(len(cords)):
         if len(cords) == 0:
             return image
         x, y, w, h = cords[i]
-        cv.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), -1)
+        if compexbg:
+            color = dominantColor(image[y:y + h, x:x + w])
+        else:
+            color = (255, 255, 255)
+        cv.rectangle(image, (x, y), (x + w, y + h), color, -1)
     return image
--- dominantColor.py
+++ dominantColor.py
@@ -1,19 +1,20 @@
 import cv2 as cv
 import numpy as np
 
-# Load the image
-img = cv.imread("image.jpg")
 
-# Convert the image to the HSV color space
-img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+def dominantColor(image):
+    # Convert the image to the HSV color space
+    img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
-# Create a histogram of the hue channel
-hist = cv.calcHist([img_hsv], [0], None, [180], [0, 180])
+    # Create a histogram of the hue channel
+    hist = cv.calcHist([img_hsv], [0], None, [180], [0, 180])
 
-# Find the index of the bin with the highest value
-most_used_color = np.argmax(hist)
+    # Find the index of the bin with the highest value
+    most_used_color = np.argmax(hist)
 
-# Convert the hue value back to BGR
-most_used_color = cv.cvtColor(np.uint8([[[most_used_color, 255, 255]]]), cv.COLOR_HSV2BGR)[0][0]
+    # Convert the hue value back to BGR
+    most_used_color = cv.cvtColor(np.uint8([[[most_used_color, 255, 255]]]), cv.COLOR_HSV2BGR)[0][0]
 
-print(most_used_color[0])
+    # convert numpy array to tuple
+    return tuple(most_used_color)
+
