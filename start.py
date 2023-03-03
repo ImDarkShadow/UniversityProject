ec9e965380888c808468f8272a6c35973bd14c91
Pritam Das <40018547+ImDarkShadow@users.noreply.github.com>
2023-03-03 22:47:03 +0530

    added loading animation

--- start.py
+++ start.py
@@ -31,20 +31,43 @@ comicFile = natsorted(comicFile)
 print(comicFile)
 images = [
     "./files/temp/" + comicName + "/" + x for x in comicFile]
-
+spinner = Halo(text='Loading Images', spinner='dots')
+spinner.start()
 img_array = read_images(images)
+spinner.succeed('Images Loaded')
+spinner = Halo(text='Concatenating Images', spinner='dots')
+spinner.start()
 image = cv.vconcat(img_array)
+spinner.succeed('Images Concatenated')
 row = image.shape[0]
 col = image.shape[1]
-
+spinner = Halo(text='Detecting Blank Areas', spinner='dots')
+spinner.start()
 crop_array = get_crop_coordinates(image, row, col)
+spinner.succeed('Blank Areas Detected')
 
+spinner = Halo(text='Crop Images Along Blank Areas', spinner='dots')
+spinner.start()
 croppedImages = crop_image(crop_array, image, col)
+spinner.succeed('Images Cropped')
+spinner = Halo(text='Detecting Text Boxes And Text', spinner='dots')
+spinner.start()
 texts, cords = ocr_images(croppedImages, lang)
+spinner.succeed('Text Boxes And Text Detected')
+spinner = Halo(text='Translating Text', spinner='dots')
+spinner.start()
 trans = translate(*texts)
+spinner.succeed('Texts Translated')
 
+spinner = Halo(text='Cleaning Raw Text', spinner='dots')
+spinner.start()
 colors = clean_raw_text(croppedImages, *cords, isComplexBG=isComplexBG)
+spinner.succeed('Raw Text Cleaned')
 
+spinner = Halo(text='Printing Translated Text', spinner='dots')
+spinner.start()
 print_comic_text(croppedImages, cords, comicName, colors, isComplexBG, trans)
-
+spinner.succeed('Translated Text Printed')
+spinner = Halo(text='Creating Zip File', spinner='dots')
 zip_files(comicName)
+spinner.succeed('Zip File Created')
